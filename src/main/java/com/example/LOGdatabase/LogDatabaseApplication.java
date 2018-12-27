b1280ab27918bd616f9cfd119cde6a096cc324bf
Hoda Serhan <hodase12@gmail.com>
2018-12-27 12:31:36 +0200

    Static content Add(47)

--- src/main/java/com/example/LOGdatabase/LogDatabaseApplication.java
+++ src/main/java/com/example/LOGdatabase/LogDatabaseApplication.java
@@ -1,14 +1,17 @@
 package com.example.LOGdatabase;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LogDatabaseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LogDatabaseApplication.class, args);
-	}
+  private static final Logger logger = LoggerFactory.getLogger(LogDatabaseApplication.class);
 
+  public static void main(String[] args) {
+    SpringApplication.run(LogDatabaseApplication.class, args);
+
+    logger.info("Hello Spring Boot");
+  }
 }
-
